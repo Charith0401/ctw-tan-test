d125d4a85aca168af415185414bd234cd25bbf3c
Charith Thiwanka Weerasinghe Mudiyanselage <114335319+Charith0401@users.noreply.github.com>
2024-07-04 02:22:20 +0530

    initial commit consisting of string reversing python file

--- ReverseString.py
+++ ReverseString.py
@@ -1,3 +1,18 @@
-def main():
-    print("test")
+def reverse_string(user_input:str)-> str:
 
+    #converting string to a list
+    string_list = list(user_input)
+
+    #using string manipulation, we are then printing all the elements
+    #in the list in reverse
+    reversed_list=string_list[::-1]
+
+    return ''.join(reversed_list)
+
+
+#
+
+stop=0
+while (stop==0):
+    user_input=str(input("Please Enter A String To Reverse: "))
+    print("Reversed String: ",reverse_string(user_input))
